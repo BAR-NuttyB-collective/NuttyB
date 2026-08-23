4217e92b1431316dc6b3ad5a33c070b21b5e105e
RandomGuyJunior <42807509+RandomGuyJunior@users.noreply.github.com>
2026-08-23 17:24:17 +0200

    Fixed Legion Drones armorclass being standard + Cortex T3 Geo in the weapon tab + New T3 Cortex Wall Model (#28)
    * Drones + Unitgroup + Model Fix
    
    Fixed Legion Drones not inheriting the vtol armordef from legdrone and counting as non air unit.
    Cortex T3 wall got a new model that makes it easley recognized.
    Cortex T3 Geo is moved from economy to the weapons tab.
    
    * Add T3 Geo and T3 Wall to T3 Builders
    
    I am surprised I didnt think of this sooner.
    
    * Remove T3 Geo and walls from T3 Builders
    
    * Add T3 builder compatibility

--- lua/geo+walls-t3.lua
+++ lua/geo+walls-t3.lua
@@ -214,7 +214,7 @@ do
         carrierdeaththroe = 'release',
         dockingarmor = 0.2,
         dockinghealrate = 256,
-        docktohealthreshold = 33,
+        docktohealthreshold = 0,
         enabledocking = false,
         dockingHelperSpeed = 5,
         dockingpieces = '10 10 10 10 10, 11 12 12',
@@ -317,6 +317,7 @@ do
         local legParadrone = cloneUnit('legdrone', 'legparadrone', {
             health = 2000,
             customparams = {
+                armordef = "vtol",
                 i18n_en_humanname = 'Reinforced Laser Drone',
             },
         })
@@ -436,6 +437,9 @@ do
     end
 
     cloneUnit('corfort', 'cortyrant', {
+        objectname = "scavs/scavfort.s3o",
+        script = "scavs/scavfort.cob",
+        buildpic = "scavengers/scavfort.dds",
         metalcost = 1300,
         energycost = 30000,
         buildtime = 60000,
@@ -492,6 +496,7 @@ do
         explodeas = 'customfusionexplo',
         selfdestructas = 'korgExplosion',
         customparams = {
+            unitgroup = "weapon",
             i18n_en_humanname = 'T3 Geo Barrage',
             i18n_en_tooltip = 'Rocket Artillery Geothermal',
             techlevel = 3,
@@ -554,6 +559,8 @@ do
         'armacsub',
         'armacv',
         'armoc',
+        'armt3airaide',
+        'armt3aide',
     }
 
     local corBuilders = {
@@ -562,6 +569,8 @@ do
         'coracsub',
         'coracv',
         'coroc',
+        'cort3airaide',
+        'cort3aide',
     }
 
     local legBuilders = {
@@ -570,6 +579,8 @@ do
         'legacv',
         'legoc',
         'leganavyconsub',
+        'legt3airaide',
+        'legt3aide',
     }
 
     for _, builderName in pairs(armBuilders) do
